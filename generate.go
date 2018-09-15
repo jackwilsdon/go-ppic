bb0105c8425d76c887bdb778c35e1a6b98b1e0f0
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-09-15 03:17:03 +0100

    Draw pixels inside separate goroutines (thanks @cookiej91!)
    This greatly improves the performance of GenerateImage;
    
    benchmark                    old ns/op     new ns/op     delta
    BenchmarkGenerateImage-8     446708        192916        -56.81%

--- generate.go
+++ generate.go
@@ -8,6 +8,7 @@ import (
 	"image"
 	"math"
 	"math/rand"
+	"sync"
 )
 
 // ErrInvalidSize is an error caused by specifying a size which is not a multiple of 8.
@@ -122,6 +123,12 @@ func GenerateImage(k string, size int, mX, mY bool, p Palette) (image.Image, err
 	img := image.NewPaletted(image.Rect(0, 0, size, size), p.Palette())
 	grid := Generate(k, mX, mY)
 
+	// Create a wait group so we can wait for all of our goroutines to finish.
+	wg := sync.WaitGroup{}
+
+	// There are going to be 64 goroutines (it's an 8x8 image).
+	wg.Add(64)
+
 	// Draw the image data onto the image.
 	for y, row := range grid {
 		for x, val := range row {
@@ -133,9 +140,15 @@ func GenerateImage(k string, size int, mX, mY bool, p Palette) (image.Image, err
 			}
 
 			// Draw the pixel.
-			rect(*img, x, y, pSize, uint8(c))
+			go func(x, y int, c uint8) {
+				rect(*img, x, y, pSize, uint8(c))
+				wg.Done()
+			}(x, y, c)
 		}
 	}
+
+	// Wait for all of the goroutines to finish.
+	wg.Wait()
 
 	return img, nil
 }
