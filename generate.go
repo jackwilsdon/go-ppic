11df7bb20653bd25b4495c3caad513ee67de40be
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-10-16 10:42:19 +0100

    Move image generation functionality into it's own file

--- generate.go
+++ generate.go
@@ -5,10 +5,8 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"image"
 	"math"
 	"math/rand"
-	"sync"
 )
 
 // ErrInvalidSize is an error caused by specifying a size which is not a multiple of 8.
@@ -23,18 +21,6 @@ func hashString(s string) int64 {
 
 	// Convert the first 8 bytes into a number.
 	return int64(binary.BigEndian.Uint64(m.Sum(nil)))
-}
-
-// rect draws a rectangle on the provided surface.
-func rect(img image.Paletted, x, y, size int, c uint8) {
-	x *= size
-	y *= size
-
-	for cY := y; cY < y+size; cY++ {
-		for cX := x; cX < x+size; cX++ {
-			img.Pix[(cY-img.Rect.Min.Y)*img.Stride+(cX-img.Rect.Min.X)] = c
-		}
-	}
 }
 
 // Generate returns an 8x8 grid of values based on the provided source text, optionally mirrored on the X or Y axis.
@@ -120,47 +106,4 @@ func Generate(k string, mX, mY bool) [8][8]bool {
 	}
 
 	return d
-}
-
-// GenerateImage returns an image for the specified source text, optionally mirrored on the X or Y axis.
-func GenerateImage(k string, size int, mX, mY bool, p Palette) (image.Image, error) {
-	if size%8 != 0 {
-		return nil, ErrInvalidSize
-	}
-
-	// The size of each pixel in the image.
-	pSize := size / 8
-
-	// Create the image and image data.
-	img := image.NewPaletted(image.Rect(0, 0, size, size), p.Palette())
-	grid := Generate(k, mX, mY)
-
-	// Create a wait group so we can wait for all of our goroutines to finish.
-	wg := sync.WaitGroup{}
-
-	// There are going to be 64 goroutines (it's an 8x8 image).
-	wg.Add(64)
-
-	// Draw the image data onto the image.
-	for y, row := range grid {
-		for x, val := range row {
-			var c uint8
-
-			// We're drawing the foreground if the value is set.
-			if val {
-				c = 1
-			}
-
-			// Draw the pixel.
-			go func(x, y int, c uint8) {
-				rect(*img, x, y, pSize, c)
-				wg.Done()
-			}(x, y, c)
-		}
-	}
-
-	// Wait for all of the goroutines to finish.
-	wg.Wait()
-
-	return img, nil
 }
---
+++ image.go
@@ -0,0 +1,61 @@
+package ppic
+
+import (
+	"image"
+	"sync"
+)
+
+// rect draws a rectangle on the provided surface.
+func rect(img image.Paletted, x, y, size int, c uint8) {
+	x *= size
+	y *= size
+
+	for cY := y; cY < y+size; cY++ {
+		for cX := x; cX < x+size; cX++ {
+			img.Pix[(cY-img.Rect.Min.Y)*img.Stride+(cX-img.Rect.Min.X)] = c
+		}
+	}
+}
+
+// GenerateImage returns an image for the specified source text, optionally mirrored on the X or Y axis.
+func GenerateImage(k string, size int, mX, mY bool, p Palette) (image.Image, error) {
+	if size%8 != 0 {
+		return nil, ErrInvalidSize
+	}
+
+	// The size of each pixel in the image.
+	pSize := size / 8
+
+	// Create the image and image data.
+	img := image.NewPaletted(image.Rect(0, 0, size, size), p.Palette())
+	grid := Generate(k, mX, mY)
+
+	// Create a wait group so we can wait for all of our goroutines to finish.
+	wg := sync.WaitGroup{}
+
+	// There are going to be 64 goroutines (it's an 8x8 image).
+	wg.Add(64)
+
+	// Draw the image data onto the image.
+	for y, row := range grid {
+		for x, val := range row {
+			var c uint8
+
+			// We're drawing the foreground if the value is set.
+			if val {
+				c = 1
+			}
+
+			// Draw the pixel.
+			go func(x, y int, c uint8) {
+				rect(*img, x, y, pSize, c)
+				wg.Done()
+			}(x, y, c)
+		}
+	}
+
+	// Wait for all of the goroutines to finish.
+	wg.Wait()
+
+	return img, nil
+}
